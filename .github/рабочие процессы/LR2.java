105b33f64b28a28cfd5d82678b833d814e05b80e
nosikovaa <121308610+nosikovaa@users.noreply.github.com>
2022-12-24 00:53:07 +0700

    Create LR2.java

---
+++ .github/рабочие процессы/LR2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class HelloWorld{
+     public static void main(String []args){
+         Scanner input = new Scanner(System.in);
+         int i=430;
+         String s="car";
+        System.out.println("Hello, World!");
+        System.out.println(i);
+        System.out.println(s);
+            s=input.nextLine();
+               System.out.println(s);
+     }
+}
